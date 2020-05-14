3fa81d95ee57fd709f8ecf78dc7c18488a47d56c
Adesanya Ayokunle <adesanyaayokunle@gmail.com>
2020-05-14 02:52:03 +0100

    prefer compositions to inheritance

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,8 @@
 import { MatchReader } from "./MatchReader";
-import { MatchResult } from "./matchResults";
+import { Summary } from "./Summary";
 
-const reader = new MatchReader("football.csv");
+const matchReader = MatchReader.fromCsv("football.csv");
+const summary = Summary.winsAnalysisWithHtmlReport("Man United");
 
-reader.read();
-
-let manUnitedWins = 0;
-
-for (const match of reader.data) {
-  if (match[1] === "Man United" && match[5] === MatchResult.HomeWin) {
-    manUnitedWins++;
-  } else if (match[2] === "Man United" && match[5] === MatchResult.AwayWin) {
-    manUnitedWins++;
-  }
-}
-
-console.log(`Man United won ${manUnitedWins} games`);
+matchReader.load();
+summary.buildAndPrintReport(matchReader.matches);
